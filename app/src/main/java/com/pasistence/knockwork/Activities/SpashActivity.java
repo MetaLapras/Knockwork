7da864770c543bca6ad7457d3be1710d9cec05fa
metalapras <channesagar@outlook.com>
2018-09-28 13:13:55 +0530

    Initial commit

---
+++ app/src/main/java/com/pasistence/knockwork/Activities/SpashActivity.java
@@ -0,0 +1,15 @@
+package com.pasistence.knockwork.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.pasistence.knockwork.R;
+
+public class SpashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spash);
+    }
+}
