8fe2b6ab634586f7325f37bfe29c75e99129489c
metalapras <channesagar@outlook.com>
2018-10-01 15:58:14 +0530

    commit

--- app/src/main/java/com/pasistence/knockwork/Activities/DashboardActivity.java
+++ app/src/main/java/com/pasistence/knockwork/Activities/DashboardActivity.java
@@ -13,6 +13,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.pasistence.knockwork.R;
+
 public class DashboardActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
