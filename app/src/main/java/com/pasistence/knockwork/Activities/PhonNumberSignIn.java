6124ec675f159cfc1430f1ac78b64440d82a02df
pranali lohave <pranalilohave94@gmail.com>
2018-10-03 12:13:15 +0530

    commit

---
+++ app/src/main/java/com/pasistence/knockwork/Activities/PhonNumberSignIn.java
@@ -0,0 +1,25 @@
+package com.pasistence.knockwork.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.pasistence.knockwork.R;
+
+public class PhonNumberSignIn extends AppCompatActivity {
+
+    TextView txtPhone;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phon_number_sign_in);
+
+        android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolBar);
+        toolbar.setTitle("Firebase Auth");
+        setSupportActionBar(toolbar);
+
+        txtPhone = (TextView)findViewById(R.id.txtPhone);
+        if (getIntent()!= null)
+            txtPhone.setText(getIntent().getStringExtra("phone"));
+    }
+}
