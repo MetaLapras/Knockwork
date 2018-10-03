9a658b360c6028da68cdcd3247f307ba36b8daa2
metalapras <channesagar@outlook.com>
2018-10-03 16:48:59 +0530

    Commited by sagar

--- app/src/main/java/com/pasistence/knockwork/Activities/DashboardActivity.java
+++ app/src/main/java/com/pasistence/knockwork/Activities/DashboardActivity.java
@@ -1,10 +1,16 @@
 package com.pasistence.knockwork.Activities;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,15 +20,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.pasistence.knockwork.Common.Common;
+import com.pasistence.knockwork.Interfaces.ItemClickListener;
+import com.pasistence.knockwork.Models.PopularServices;
 import com.pasistence.knockwork.R;
+import com.pasistence.knockwork.ViewHolder.ViewHolderPopularServices;
+import com.squareup.picasso.Picasso;
 
 public class DashboardActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     Context mContext;
     RecyclerView recyclerPopularServices,recyclerTopServices;
-    RecyclerView.LayoutManager layoutManager ;
+    RecyclerView.LayoutManager GridlayoutManager,LinearLayoutManager ;
+    SwipeRefreshLayout refreshLayout;
+
+
+    FirebaseRecyclerAdapter<PopularServices,ViewHolderPopularServices> popularAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +49,7 @@ public class DashboardActivity extends AppCompatActivity
         setContentView(R.layout.activity_dashboard);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        mInit();
 
         /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -48,6 +68,109 @@ public class DashboardActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+
+        refreshLayout.setColorSchemeResources(R.color.colorPrimary,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_dark,
+                android.R.color.holo_blue_dark);
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                //to Load menu from Firebase
+                if(Common.isConnectedToInterNet(getBaseContext())) {
+                    loadPopularServices();
+                    loadTopServices();
+                }else
+                {
+                    Toast.makeText(getBaseContext(), "Check Your Internet Connection ! ", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+        });
+        //Default Load
+        refreshLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                //to Load menu from Firebase
+                if(Common.isConnectedToInterNet(getBaseContext())) {
+                    loadPopularServices();
+                    loadTopServices();
+                }else
+                {
+                    Toast.makeText(getBaseContext(), "Check Your Internet Connection ! ", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+            }
+        });
+
+
+        Common.InitFirebase("popular");
+        FirebaseRecyclerOptions<PopularServices> options = new FirebaseRecyclerOptions.Builder<PopularServices>()
+                .setQuery(Common.databaseReference,PopularServices.class)
+                .build();
+
+        popularAdapter = new FirebaseRecyclerAdapter<PopularServices, ViewHolderPopularServices>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull ViewHolderPopularServices viewHolder, int position, @NonNull PopularServices model) {
+                viewHolder.txtPopularservice.setText(model.getName());
+                Picasso.with(getBaseContext()).load(model.getImage())
+                        .into(viewHolder.imgPopularservice);
+
+                final PopularServices clickitem = model;
+
+                viewHolder.setItemClickListener(new ItemClickListener() {
+                    @Override
+                    public void onClick(View view, int position, boolean isLongClick) {
+                       /* Intent intent = new Intent(Home.this, FoodMenu.class);
+                        intent.putExtra("CategoryId",adapter.getRef(position).getKey());
+                        startActivity(intent);*/
+                        Toast.makeText(mContext,""+clickitem.getName(),Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+
+            @Override
+            public ViewHolderPopularServices onCreateViewHolder(ViewGroup parent, int viewType) {
+                View itemView = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.custome_template_popularservices,parent,false);
+                return new ViewHolderPopularServices(itemView);
+
+            }
+        };
+
+
+
+    }
+
+    private void loadTopServices() {
+
+    }
+
+    private void loadPopularServices() {
+        popularAdapter.notifyDataSetChanged();
+        popularAdapter.startListening();
+        recyclerPopularServices.setAdapter(popularAdapter);
+        recyclerPopularServices.scheduleLayoutAnimation();
+        refreshLayout.setRefreshing(false);
+
+    }
+
+    private void mInit() {
+        refreshLayout = (SwipeRefreshLayout)findViewById(R.id.swipe_refresh_layout);
+
+        recyclerPopularServices =(RecyclerView)findViewById(R.id.recycler_popular_services);
+        recyclerPopularServices.setHasFixedSize(true);
+        LinearLayoutManager     = new LinearLayoutManager(this);
+        recyclerPopularServices.setLayoutManager(GridlayoutManager);
+
+        recyclerTopServices     =(RecyclerView)findViewById(R.id.recycler_top_services);
+        recyclerTopServices.setHasFixedSize(true);
+        GridlayoutManager       = new GridLayoutManager(this,2);
+        recyclerTopServices.setLayoutManager(GridlayoutManager);
+
     }
 
     @Override
---
+++ app/src/main/java/com/pasistence/knockwork/ViewHolder/ViewHolderTopServices.java
@@ -0,0 +1,16 @@
+package com.pasistence.knockwork.ViewHolder;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class ViewHolderTopServices extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public ViewHolderTopServices(@NonNull View itemView) {
+        super(itemView);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
