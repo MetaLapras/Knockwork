da617f2f3d404ad4503536d1c687c303955ee2e0
pranali lohave <pranalilohave94@gmail.com>
2018-10-04 18:52:38 +0530

    commit

---
+++ app/src/main/java/com/pasistence/knockwork/Employeer/ViewHolder/ViewHolderInboxList.java
@@ -0,0 +1,30 @@
+package com.pasistence.knockwork.Employeer.ViewHolder;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.pasistence.knockwork.R;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class ViewHolderInboxList extends RecyclerView.ViewHolder  {
+
+    public CircleImageView CircularImageViewProfile;
+    public TextView txtInboxListName,txtLancerDescription,txtICanDo;
+
+
+    public ViewHolderInboxList(@NonNull View itemView) {
+        super(itemView);
+
+        CircularImageViewProfile = (CircleImageView)itemView.findViewById(R.id.inbox_image_view);
+        txtInboxListName            = (TextView)itemView.findViewById(R.id.inbox_member_name);
+        txtLancerDescription           = (TextView)itemView.findViewById(R.id.inbx_member_description);
+        txtICanDo          = (TextView)itemView.findViewById(R.id.inbox_i_can_do);
+
+    }
+}
+
