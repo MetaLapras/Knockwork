704c575b12ab0b9977a2aec5d8db34e590461f0d
AshClan <aashaykarekar@gmail.com>
2018-10-31 20:46:56 +0530

    c

--- app/src/main/java/com/pasistence/knockwork/ChatBox/DummyData/DialogsFixtures.java
+++ app/src/main/java/com/pasistence/knockwork/ChatBox/DummyData/DialogsFixtures.java
@@ -1,4 +1,16 @@
 package com.pasistence.knockwork.ChatBox.DummyData;
 
-public class DialogsFixtures {
+import com.pasistence.knockwork.ChatBox.ChatModel.Dialog;
+
+import java.util.ArrayList;
+
+public class DialogsFixtures extends FixturesData {
+
+    private DialogsFixtures(){
+        throw new AssertionError();
+    }
+//
+//    public static ArrayList<Dialog> getDialogs(){
+//
+//    }
 }
