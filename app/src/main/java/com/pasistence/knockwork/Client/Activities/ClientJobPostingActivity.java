08eefdb5f07d3b57d2e0a78966a63db851dfd31f
pranali lohave <pranalilohave94@gmail.com>
2018-10-31 16:21:25 +0530

    commit

--- app/src/main/java/com/pasistence/knockwork/Client/Activities/ClientJobPostingActivity.java
+++ app/src/main/java/com/pasistence/knockwork/Client/Activities/ClientJobPostingActivity.java
@@ -312,11 +312,11 @@ public class ClientJobPostingActivity extends AppCompatActivity implements View.
             cancel=true;
         }
 
-       /* if (TextUtils.isEmpty(editINR.getText())){
+        if (TextUtils.isEmpty(editINR.getText())){
             editINR.setError("Please enter min 5 Skills* ");
             focusView=editINR;
             cancel=true;
-        }*/
+        }
 
         if(spnDone.getSelectedItemPosition() == 0)
         {
