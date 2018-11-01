91faacc46c37eb197859cd4f070ad904eadd30df
pranali lohave <pranalilohave94@gmail.com>
2018-11-01 17:17:03 +0530

    commit

--- app/src/main/java/com/pasistence/knockwork/Adapter/ManageJobPostingAdapter.java
+++ app/src/main/java/com/pasistence/knockwork/Adapter/ManageJobPostingAdapter.java
@@ -1,18 +1,26 @@
 package com.pasistence.knockwork.Adapter;
 
+import android.app.Activity;
+import android.app.ActivityOptions;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Parcelable;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.Toast;
 
 import com.pasistence.knockwork.Client.Activities.ClientJobPostingActivity;
+import com.pasistence.knockwork.Client.Activities.ManageJobPostActivity;
 import com.pasistence.knockwork.Common.Common;
+import com.pasistence.knockwork.Common.PreferenceUtils;
+import com.pasistence.knockwork.LoginActivity;
 import com.pasistence.knockwork.Model.ApiResponse.ApiPostJobResponse;
 import com.pasistence.knockwork.Model.ManageJobPostingModel;
 import com.pasistence.knockwork.R;
@@ -32,18 +40,17 @@ public class ManageJobPostingAdapter extends RecyclerView.Adapter<ViewHolderMnag
 
     private static final String TAG = "managePostingAdapter";
     Context mContext;
+    Activity activity;
     ArrayList<ApiPostJobResponse.Result> manageJobPostingModels ;
     ApiPostJobResponse.Result job;
     MyApi mServices;
-    ManageJobPostingAdapter manageJobPostingAdapter;
 
-
-    public ManageJobPostingAdapter(Context mContext, ArrayList<ApiPostJobResponse.Result> workerList) {
+    public ManageJobPostingAdapter(Context mContext,Activity activity, ArrayList<ApiPostJobResponse.Result> workerList) {
         this.mContext = mContext;
         this.manageJobPostingModels = workerList;
+        this.activity = activity;
         mServices = Common.getApi();
     }
-
 
     @NonNull
     @Override
@@ -85,51 +92,90 @@ public class ManageJobPostingAdapter extends RecyclerView.Adapter<ViewHolderMnag
         holder.btnJobRemove.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(!job.getPId().equals(null) && !job.getCid().equals(null) && !job.getUid().equals(null))
-                {
-                    Common.showSpotDialogue(mContext);
-                    try {
-                        mServices.ClientPostAJobDelete(job.getPId(),job.getUid(),job.getCid()).enqueue(new Callback<ApiPostJobResponse>() {
-                            @Override
-                            public void onResponse(Call<ApiPostJobResponse> call, Response<ApiPostJobResponse> response) {
-                                response.body().getMessage();
-                                ApiPostJobResponse result = response.body();
-                                Log.e(TAG, result.toString());
-                                if(!result.getError()){
-                                    Common.dismissSpotDilogue();
-                                    notifyDataSetChanged();
-                                }else if(result.getError()){
-                                    Common.commonDialog(mContext,result.getMessage());
-                                    Common.dismissSpotDilogue();
-                                }else {
-                                    Common.commonDialog(mContext,"Sever not found..");
-                                    Common.dismissSpotDilogue();
-                                }
-                            }
 
-                            @Override
-                            public void onFailure(Call<ApiPostJobResponse> call, Throwable t) {
-                                Log.e(TAG, t.getMessage() );
-                                t.printStackTrace();
-                               // Common.dismissSpotDilogue();
-                                Common.commonDialog(mContext,"Something went, wrong please try again");
+                removeAt(position);
+
+            }
+        });
+    }
+
+    private void removeAt(final int position) {
+        if(!job.getPId().equals(null) && !job.getCid().equals(null) && !job.getUid().equals(null))
+        {
+
+            try {
+
+                //Common.dismissSpotDilogue();
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(mContext);
+                alertDialogBuilder.setMessage("Are you Sure Want to Delete")
+                        .setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+//                                                   notifyDataSetChanged();
+
+                                mServices.ClientPostAJobDelete(job.getPId(),job.getUid(),job.getCid()).enqueue(new Callback<ApiPostJobResponse>() {
+                                    @Override
+                                    public void onResponse(Call<ApiPostJobResponse> call, Response<ApiPostJobResponse> response) {
+                                        response.body().getMessage();
+                                        final ApiPostJobResponse result = response.body();
+                                        Log.e(TAG, result.toString());
+                                        if(!result.getError()){
+                                            manageJobPostingModels.remove(position);
+                                            notifyItemRemoved(position);
+                                            notifyItemRangeChanged(position,manageJobPostingModels.size());
+
+                                            //Intent intent = new Intent(mContext,ManageJobPostActivity.class);
+                                            //ActivityOptions options = ActivityOptions.makeCustomAnimation(mContext,android.R.anim.fade_in,android.R.anim.fade_out);
+                                            //mContext.startActivity(intent,options.toBundle());
+                                            //activity.finish();
+                                            //mContext.startActivity(new Intent(mContext,ManageJobPostActivity.class));
+
+                                        }else if(result.getError()){
+                                            Common.commonDialog(mContext,result.getMessage());
+                                            Common.dismissSpotDilogue();
+                                        }else {
+                                            Common.commonDialog(mContext,"Sever not found..");
+                                            Common.dismissSpotDilogue();
+                                        }
+                                        //  Common.dismissSpotDilogue();
+                                    }
+
+                                    @Override
+                                    public void onFailure(Call<ApiPostJobResponse> call, Throwable t) {
+                                        Log.e(TAG, t.getMessage() );
+                                        t.printStackTrace();
+                                        Common.dismissSpotDilogue();
+                                        Common.commonDialog(mContext,"Something went, wrong please try again");
+                                    }
+                                });
+
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // User cancelled the dialog
+                                dialog.dismiss();
                             }
                         });
 
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
 
-                    }catch (Exception e)
-                    {
-                        Log.e(TAG, e.getMessage() );
-                        e.printStackTrace();
-                        Common.commonDialog(mContext,"Sever not found..");
-                    }
-                }
+
+
+            }catch (Exception e)
+            {
+                Log.e(TAG, e.getMessage() );
+                e.printStackTrace();
+                Common.commonDialog(mContext,"Sever not found..");
             }
-        });
+        }
+
     }
 
     @Override
     public int getItemCount() {
         return manageJobPostingModels.size();
     }
+
+
 }
