cdcc00ffd5dd27e04dea6506ef3cb2232962213e
AshClan <aashaykarekar@gmail.com>
2018-11-01 17:16:05 +0530

    k

--- app/src/main/java/com/pasistence/knockwork/ChatBox/BaseApplication.java
+++ app/src/main/java/com/pasistence/knockwork/ChatBox/BaseApplication.java
@@ -2,11 +2,6 @@ package com.pasistence.knockwork.ChatBox;
 
 import android.app.Application;
 
-<<<<<<< HEAD
-=======
-/*import com.sendbird.android.SendBird;*/
->>>>>>> 527df492c2baa18983919ba978ff74ae56a50f15
-
 public class BaseApplication extends Application {
     private static final String APP_ID = "8198AB3F-0720-4858-BA20-BB0305646B02";
     public static final String VERSION = "1.0.1";
@@ -15,9 +10,5 @@ public class BaseApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
-<<<<<<< HEAD
-=======
-        //SendBird.init(APP_ID, getApplicationContext());
->>>>>>> 527df492c2baa18983919ba978ff74ae56a50f15
     }
 }
